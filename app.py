e3a0ba15ac6c4647c9227e1f91a462867cb9b423
pj915054-cmd <pj915054@gmail.com>
2026-08-27 09:11:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 def add(a, b):
     return a + b
 
-print(add(10, 20))
+# print(add(10, 20)) 
+
+def subtract(a,b):
+    return a-b
+
+def multiple(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
