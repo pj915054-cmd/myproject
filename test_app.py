4f084f5d6e62beb07f5be9aa60c8e5b1d22215d7
pj915054-cmd <pj915054@gmail.com>
2026-08-27 09:13:32 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,14 @@
-from app import add
+from app import add,subtract,mutiple,divide
 
 def test_add():
     assert add(10, 20) == 30
+
+def test_subtract():
+    assert subtract(10,20) == -10
+
+def test_mutiple():
+    assert mutiple(10,20) == 300
+
+def test_divide():
+    assert divide(10,20) == 0
+    
